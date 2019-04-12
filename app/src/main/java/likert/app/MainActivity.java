e5ddc059c1643edec5cd3be484687a911b990d3a
llimanor <lukaslymauchiha@gmail.com>
2019-04-12 15:29:07 -0300

    RecyclerView

--- app/src/main/java/likert/app/MainActivity.java
+++ app/src/main/java/likert/app/MainActivity.java
@@ -1,18 +1,17 @@
 package likert.app;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.util.TypedValue;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TableLayout;
@@ -20,10 +19,21 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import likert.app.Util.AddArrays;
+
+import static likert.app.Util.Constant.BAD;
+import static likert.app.Util.Constant.LIKE;
+import static likert.app.Util.Constant.LOVE;
+import static likert.app.Util.Constant.MAX;
+import static likert.app.Util.Constant.NOT_KNOW;
+import static likert.app.Util.Constant.NOT_LIKE;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView reaction1, reaction2, reaction3, reaction4, reaction5;
+    private ImageView allImages[];
     private TextView textReaction1, textReaction2, textReaction3, textReaction4, textReaction5;
+    private TextView allTexts[];
     private AppCompatEditText editText;
     private TextView counterText;
     private TextView question;
@@ -45,40 +55,114 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TableLayout tableLayout;
 
+    private Button prox;
+
+    private AddArrays addArrays;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        addArrayList();
         registerClick();
 
-        final int max = 140;
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                if (count <= max) {
-                    //counterText.setText("" + (max - count) + " caracteres");
+        allImages = new ImageView[]{reaction1, reaction2, reaction3, reaction4, reaction5};
+        allTexts = new TextView[]{textReaction1, textReaction2, textReaction3, textReaction4, textReaction5};
+
+        addArrays = new AddArrays(this);
+        addArrays.addArrayList();
+        addArrayList();
+    }
+
+    private void registerClick() {
+        reaction1.setOnClickListener(this);
+        reaction2.setOnClickListener(this);
+        reaction3.setOnClickListener(this);
+        reaction4.setOnClickListener(this);
+        reaction5.setOnClickListener(this);
+        send.setOnClickListener(this);
+        prox.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.reaction1:
+                defaultReactions(BAD);
+                break;
+            case R.id.reaction2:
+                defaultReactions(NOT_LIKE);
+                break;
+            case R.id.reaction3:
+                defaultReactions(NOT_KNOW);
+                break;
+            case R.id.reaction4:
+                defaultReactions(LIKE);
+                break;
+            case R.id.reaction5:
+                defaultReactions(LOVE);
+                break;
+            case R.id.send:
+                tableLayout.setVisibility(View.GONE);
+                layout.setVisibility(View.GONE);
+                if (!switchResult.isChecked()) {
+
+                    SpannableStringBuilder title_string = new SpannableStringBuilder(getString(R.string.success_text));
+                    int color = getResources().getColor(android.R.color.holo_orange_dark);
+                    ForegroundColorSpan fcs = new ForegroundColorSpan(color);
+                    title_string.setSpan(fcs, 20, title_string.length(), 0);
+
+                    title.setText(title_string);
+                    layoutResult.setVisibility(View.VISIBLE);
+                    layoutResult.setBackgroundColor(getResources().getColor(R.color.branco));
+                    textInfo.setText(getString(R.string.success_body));
+                    refresh.setVisibility(View.GONE);
                 } else {
-                    //counterText.setText("0 caracteres");
+                    title.setText(getString(R.string.error_text));
+                    layoutResult.setVisibility(View.VISIBLE);
+                    layoutResult.setBackgroundColor(getResources().getColor(R.color.cinza));
+                    textInfo.setText(getString(R.string.error_body));
+                    refresh.setVisibility(View.VISIBLE);
                 }
+                break;
+            case R.id.prox:
+                Intent intent = new Intent(this, MainRecycle.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void defaultReactions(int position) {
+        for (int i = 0; i <= 4; i++) {
+            if (i == position) {
+                imageViews.get(i).setImageDrawable(imagesColor.get(i));
+                textViews.get(i).setTypeface(null, Typeface.BOLD);
+                question.setText(textQuestions.get(i));
+            } else {
+                imageViews.get(i).setImageDrawable(imagesDefault.get(i));
+                textViews.get(i).setTypeface(null, Typeface.NORMAL);
             }
+        }
+        layout.setVisibility(View.VISIBLE);
+    }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (count <= max) {
-                    //counterText.setText("" + (max - count) + " caracteres");
-                } else {
-                    //counterText.setText("0 caracteres");
-                }
-            }
+    public void addArrayList() {
+        for (int i = 0; i < MAX; i++) {
+            imageViews.add(allImages[i]);
+            textViews.add(allTexts[i]);
+            imagesDefault.add(addArrays.getImagesDefault().get(i));
+            imagesColor.add(addArrays.getImagesColor().get(i));
+            textQuestions.add(addArrays.getTextQuestions().get(i));
+        }
+    }
 
-            @Override
-            public void afterTextChanged(Editable s) {
 
-            }
-        });
+    public void sizeImage(int i) {
+        int dimensionInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45, getResources().getDisplayMetrics());
 
+        imageViews.get(i).getLayoutParams().height = dimensionInDp;
+        imageViews.get(i).getLayoutParams().width = dimensionInDp;
+        imageViews.get(i).requestLayout();
     }
 
     public void init() {
@@ -109,107 +193,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tableLayout = (TableLayout) findViewById(R.id.table);
 
-    }
+        prox = (Button) findViewById(R.id.prox);
 
-    private void registerClick() {
-        reaction1.setOnClickListener(this);
-        reaction2.setOnClickListener(this);
-        reaction3.setOnClickListener(this);
-        reaction4.setOnClickListener(this);
-        reaction5.setOnClickListener(this);
-        send.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.reaction1:
-                defaultReactions(0);
-                break;
-            case R.id.reaction2:
-                defaultReactions(1);
-                break;
-            case R.id.reaction3:
-                defaultReactions(2);
-                break;
-            case R.id.reaction4:
-                defaultReactions(3);
-                break;
-            case R.id.reaction5:
-                defaultReactions(4);
-                break;
-            case R.id.send:
-                tableLayout.setVisibility(View.GONE);
-                layout.setVisibility(View.GONE);
-                if (!switchResult.isChecked()) {
-                    title.setText("obrigado por dedicar\no seu tempo ;)");
-                    layoutResult.setVisibility(View.VISIBLE);
-                    layoutResult.setBackgroundColor(getResources().getColor(R.color.branco));
-                    textInfo.setText("vamos analisar o seu comentário\npara deixar o app Itaú ainda\nmelhor para você.");
-                    refresh.setVisibility(View.GONE);
-                } else {
-                    title.setText("desculpe, não foi\npossível enviar sua\navaliação :(");
-                    layoutResult.setVisibility(View.VISIBLE);
-                    layoutResult.setBackgroundColor(getResources().getColor(R.color.cinza));
-                    textInfo.setText("verifique sua conexão de\ninternet e tente novamente");
-                    refresh.setVisibility(View.VISIBLE);
-                }
-                break;
-        }
-    }
-
-    private void defaultReactions(int position) {
-        for (int i = 0; i <= 4; i++) {
-            if (i == position) {
-                imageViews.get(i).setImageDrawable(imagesColor.get(i));
-                textViews.get(i).setTypeface(null, Typeface.BOLD);
-                question.setText(textQuestions.get(i));
-            } else {
-                imageViews.get(i).setImageDrawable(imagesDefault.get(i));
-                textViews.get(i).setTypeface(null, Typeface.NORMAL);
-            }
-        }
-        layout.setVisibility(View.VISIBLE);
-    }
-
-    public void addArrayList() {
-        imageViews.add(reaction1);
-        imageViews.add(reaction2);
-        imageViews.add(reaction3);
-        imageViews.add(reaction4);
-        imageViews.add(reaction5);
-
-        textViews.add(textReaction1);
-        textViews.add(textReaction2);
-        textViews.add(textReaction3);
-        textViews.add(textReaction4);
-        textViews.add(textReaction5);
-
-        imagesDefault.add(getResources().getDrawable(R.drawable.ruiminativo));
-        imagesDefault.add(getResources().getDrawable(R.drawable.naogosteiinativo));
-        imagesDefault.add(getResources().getDrawable(R.drawable.seilainativo));
-        imagesDefault.add(getResources().getDrawable(R.drawable.gosteiinativo));
-        imagesDefault.add(getResources().getDrawable(R.drawable.adoreiinativo));
-
-        imagesColor.add(getResources().getDrawable(R.drawable.ruimativo));
-        imagesColor.add(getResources().getDrawable(R.drawable.naogosteiativo));
-        imagesColor.add(getResources().getDrawable(R.drawable.seilaativo));
-        imagesColor.add(getResources().getDrawable(R.drawable.gosteiativo));
-        imagesColor.add(getResources().getDrawable(R.drawable.adoreiativo));
-
-        textQuestions.add("o que foi ruim?");
-        textQuestions.add("o que você não gostou?");
-        textQuestions.add("o que você achou?");
-        textQuestions.add("o que você gostou?");
-        textQuestions.add("o que você adorou?");
-    }
-
-
-    public void sizeImage(int i) {
-        int dimensionInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45, getResources().getDisplayMetrics());
-
-        imageViews.get(i).getLayoutParams().height = dimensionInDp;
-        imageViews.get(i).getLayoutParams().width = dimensionInDp;
-        imageViews.get(i).requestLayout();
     }
 }
